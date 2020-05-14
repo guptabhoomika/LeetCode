6655e66f8d0384d9ea2cdf33dd0c89d8f1bfe66e
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-05-14 12:08:15 +0530

    day13

---
+++ day11.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+         HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        // Traverse through array elements and 
+        // count frequencies 
+        //element key count vale
+        for(int i =0;i<nums.length;i++)
+        {
+            if(mp.containsKey(nums[i]))
+            {
+                mp.put(nums[i], mp.get(nums[i]) + 1);
+            }
+            else
+            {
+                mp.put(nums[i], 1);
+            }
+            
+            
+        }
+        //checks the condition
+        for(int k =0;k<nums.length;k++)
+        {
+            int m = mp.get(nums[k]);
+            if(m !=2)
+            {
+                return nums[k];
+            }
+        }
+        return 0;
+        
+    }
+        
+    }
